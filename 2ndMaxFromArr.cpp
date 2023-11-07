eac36562980c0413bc23d3b90a3254c7be886b82
Raj Jadhav <raj20mff@gmail.com>
2023-11-07 19:12:19 +0530

    first commit

---
+++ 2ndMaxFromArr.cpp
@@ -0,0 +1,21 @@
+// Calculating the maximum and second maximum element from the array 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0 ; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    // for(int i = 0 ; i < n; i++)
+    // {
+    cout << arr[n-2];
+    //}
+}
+
